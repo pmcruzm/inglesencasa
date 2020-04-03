4abda9e75f610aaa81725ecb0088e3fd1217a8b0
David Mateos <damarev@gmail.com>
2020-04-03 14:22:15 +0200

    Facebook pixel

--- js/application-cookies.js
+++ js/application-cookies.js
@@ -1,8 +1,9 @@
 jQuery(document).on('ready',function(){
 
-	var app_id = jQuery('meta[property="app-id"]').attr('content');
-	var ga_id = jQuery('meta[property="google-analytics-id"]').attr('content');
-	var cookie_consent_name = app_id + '-cookies';
+	var appid = jQuery('meta[property="app-id"]').attr('content'),
+		ga_id = jQuery('meta[property="google-analytics-id"]').attr('content'),
+		fb_id = jQuery('meta[property="facebook-pixel-id"]').attr('content'),
+		cookie_consent_name = appid + '-cookies';
 
 	//Miramos si la cookie de aceptación está creada
 	if(jQuery.cookie(cookie_consent_name) == 'acepta'){
@@ -10,6 +11,7 @@ jQuery(document).on('ready',function(){
 		jQuery('.block-cookies').hide();
 		//Inicializamos GoogleAnalytics
 		initGoogleAnalytics(ga_id);
+		initFacebookPixel(fb_id);
 	}else{
 		jQuery('.block-cookies').show();
 	}
@@ -27,13 +29,12 @@ jQuery(document).on('ready',function(){
 		jQuery('.block-cookies').fadeOut(300);
 		//Inicializamos GoogleAnalytics
 		initGoogleAnalytics(ga_id);
+		initFacebookPixel(fb_id);
 	});
 
 });
 
-
 function initGoogleAnalytics(ga_id) {
-
 	var s = document.createElement("script");
 		s.type = "text/javascript";
 		s.src = "https://www.googletagmanager.com/gtag/js?id=" + ga_id;
@@ -44,4 +45,27 @@ function initGoogleAnalytics(ga_id) {
 	function gtag(){dataLayer.push(arguments);}
 	gtag('js', new Date());
 	gtag('config', ga_id);
+
+	return;
+
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', ga_id, 'auto');
+	ga('send', 'pageview');
+}
+
+function initFacebookPixel(fb_id) {
+	!function(f,b,e,v,n,t,s)
+	{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t,s)}(window,document,'script',
+	'https://connect.facebook.net/en_US/fbevents.js');
+	fbq('init', fb_id);
+	fbq('track', 'PageView');
 }
--- js/application-cookies_new.js
+++ js/application-cookies_new.js
@@ -1,75 +1,30 @@
-var ACEPTA_COOKIES_NAME  = 'cambridge-inglesencasa-acepta-cookies';
-
 jQuery(document).on('ready',function(){
+	window.__cmp('getGooglePersonalization', function(consent, isSuccess) {
+		if(!isSuccess)
+	 		return;
 
-	//Miramos si la cookie de aceptación está creada
-	/*if(jQuery.cookie(ACEPTA_COOKIES_NAME) == 'acepta'){
-		//Ocultamos info cookies
-		jQuery('.block-cookies').hide();
-		//Inicializamos GoogleAnalytics
-		initGoogleAnalytics();
-	}else{
-		jQuery('.block-cookies').show();
-	}
-
-	//Cerrar cuadro info cookies
-	jQuery(document).on('click','.close_c',function(event){
-		event.preventDefault();
-		jQuery('.block-cookies').fadeOut(300);
+		if(consent.googlePersonalizationData.consentValue) {
+		  //
+		  // You have consent from the user:
+		  // add your code here to call google’s admanager or adsense
+		  //
+		  	var ga_id = jQuery('meta[property="google-analytics-id"]').attr('content');
+			var s = document.createElement("script");
+				s.type = "text/javascript";
+				s.src = "https://www.googletagmanager.com/gtag/js?id=" + ga_id;
+				s.setAttribute('async','');
+				jQuery("head").append(s);
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', ga_id);
+		} else {
+		  //
+		  // No consent for personalized ads from the user:
+		  // either no call to google’s admanger / adsense or
+		  // call admanager and adsense using the appropriate
+		  // method to set ‘requestNonPersonalizedAds’ accordingly.
+		  //
+		}
 	});
-
-	//Aceptar cookies en el cuadro
-	jQuery(document).on('click','.btn-accept',function(e){
-		e.preventDefault();
-		jQuery.cookie(ACEPTA_COOKIES_NAME, 'acepta', { expires: 365 * 10 ,path: '/' });
-		jQuery('.block-cookies').fadeOut(300);
-		//Inicializamos GoogleAnalytics
-		initGoogleAnalytics();
-	});*/
-
-	//Miramos si la cookie de aceptación está creada
-	__cmp('getGooglePersonalization', function(consent, isSuccess) {
-
-	// do we have a cookie?
-	if(!isSuccess)
-	 return;
-
-	// check for given consent
-	if(consent.googlePersonalizationData.consentValue) {
-	  //
-	  // You have consent from the user:
-	  // add your code here to call google’s admanager or adsense
-	  //
-	  initGoogleAnalytics();
-	} else {
-	  //
-	  // No consent for personalized ads from the user:
-	  // either no call to google’s admanger / adsense or
-	  // call admanager and adsense using the appropriate
-	  // method to set ‘requestNonPersonalizedAds’ accordingly.
-	  //
-	}
-	});
-
-
 });
-
-
-function initGoogleAnalytics() {
-
-	var GA_ID = jQuery('meta[property="google-analytics-id"]').attr('content');
-
-	if(GA_ID && GA_ID != '') {
-		var s = document.createElement("script");
-			s.type = "text/javascript";
-			s.src = "https://www.googletagmanager.com/gtag/js?id=" + GA_ID;
-			s.setAttribute('async','');
-			jQuery("head").append(s);
-
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
-
-		gtag('config', GA_ID);
-	}
-}
