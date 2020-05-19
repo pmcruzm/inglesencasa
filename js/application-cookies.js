320f9634a2e07a6337c374ee9d2a358c83ef5fbd
David Mateos <damarev@gmail.com>
2020-05-19 12:19:48 +0200

    GA removed

--- js/application-cookies.js
+++ js/application-cookies.js
@@ -1,70 +1,41 @@
 jQuery(document).on('ready',function(){
 
 	var appid = jQuery('meta[property="app-id"]').attr('content'),
-		ga_id = jQuery('meta[property="google-analytics-id"]').attr('content'),
-		fb_id = jQuery('meta[property="facebook-pixel-id"]').attr('content'),
 		cookie_consent_name = appid + '-cookies';
 
-	//Miramos si la cookie de aceptación está creada
 	if(jQuery.cookie(cookie_consent_name) == 'acepta'){
-		//Ocultamos info cookies
 		jQuery('.block-cookies').hide();
-		//Inicializamos GoogleAnalytics
-		initGoogleAnalytics(ga_id);
-		initFacebookPixel(fb_id);
+		initAnalytics();
 	}else{
 		jQuery('.block-cookies').show();
 	}
 
-	//Cerrar cuadro info cookies
 	jQuery(document).on('click','.close_c',function(event){
 		event.preventDefault();
 		jQuery('.block-cookies').fadeOut(300);
 	});
 
-	//Aceptar cookies en el cuadro
 	jQuery(document).on('click','.btn-accept',function(e){
 		e.preventDefault();
 		jQuery.cookie(cookie_consent_name, 'acepta', { expires: 365 * 10 ,path: '/' });
 		jQuery('.block-cookies').fadeOut(300);
-		//Inicializamos GoogleAnalytics
-		initGoogleAnalytics(ga_id);
-		initFacebookPixel(fb_id);
+		initAnalytics();
 	});
-
 });
 
-function initGoogleAnalytics(ga_id) {
-	var s = document.createElement("script");
-		s.type = "text/javascript";
-		s.src = "https://www.googletagmanager.com/gtag/js?id=" + ga_id;
-		s.setAttribute('async','');
-		jQuery("head").append(s);
+function initAnalytics() {
 
-	window.dataLayer = window.dataLayer || [];
-	function gtag(){dataLayer.push(arguments);}
-	gtag('js', new Date());
-	gtag('config', ga_id);
+	var	ga_id = jQuery('meta[property="google-analytics-id"]').attr('content');
+	var fb_id = jQuery('meta[property="facebook-pixel-id"]').attr('content');
 
-
+	//GTM
 	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 	})(window,document,'script','dataLayer','GTM-5F9T9CL');
 
-	return;
-
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	ga('create', ga_id, 'auto');
-	ga('send', 'pageview');
-}
-
-function initFacebookPixel(fb_id) {
+	// Facebook Pixel
 	!function(f,b,e,v,n,t,s)
 	{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
