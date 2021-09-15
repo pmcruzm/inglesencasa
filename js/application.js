57e533625fec75f95668c22df7ccf82a1917a793
David Mateos <damarev@gmail.com>
2021-09-15 12:40:36 +0200

    Fix video thumbs

--- js/application.js
+++ js/application.js
@@ -321,7 +321,14 @@ jQuery(document).on('ready',function(){
         jQuery(this).closeModal();
     });
 
-    jQuery('.mfp').magnificPopup();
+    jQuery('.mfp').magnificPopup({
+    	iframe: {
+		  markup: '<div class="mfp-iframe-scaler">'+
+		            '<div class="mfp-close"></div>'+
+		            '<iframe class="mfp-iframe" frameborder="0" allow="autoplay" allowfullscreen></iframe>'+
+		          '</div>'
+		}
+    });
 
 });
 
